cdfd0d3aa0bdfc250f29a00b7746cc06d4aa964c
Brian Wangila <104423183+BrianWangila@users.noreply.github.com>
2023-02-02 12:38:31 +0300

    Updating DB Connection

--- config/database.php
+++ config/database.php
@@ -44,23 +44,28 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'Database'=> 'mysql',
+            'Server' => 'laravelapiravvy-server.mysql.database.azure.com',
+            'User Id' => 'upooeaawfx',
+            'Password' => '4I1JC486PWOXR34G$'
+            
+//             'driver' => 'mysql',
+//             'url' => env('DATABASE_URL'),
+//             'host' => env('DB_HOST', '127.0.0.1'),
+//             'port' => env('DB_PORT', '3306'),
+//             'database' => env('DB_DATABASE', 'forge'),
+//             'username' => env('DB_USERNAME', 'forge'),
+//             'password' => env('DB_PASSWORD', ''),
+//             'unix_socket' => env('DB_SOCKET', ''),
+//             'charset' => 'utf8mb4',
+//             'collation' => 'utf8mb4_unicode_ci',
+//             'prefix' => '',
+//             'prefix_indexes' => true,
+//             'strict' => true,
+//             'engine' => null,
+//             'options' => extension_loaded('pdo_mysql') ? array_filter([
+//                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+//             ]) : [],
         ],
 
         'pgsql' => [
