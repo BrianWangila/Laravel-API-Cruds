b158fbd0b2c5efd93fd28e6495766d259fb31594
Brian Wangila <104423183+BrianWangila@users.noreply.github.com>
2023-02-03 08:15:05 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -44,18 +44,18 @@ return [
         ],
 
         'mysql' => [
-            'Database'=> 'mysql',
-            'Server' => 'laravelapiravvy-server.mysql.database.azure.com',
-            'User Id' => 'upooeaawfx',
-            'Password' => '4I1JC486PWOXR34G$'
+//             'Database'=> 'mysql',
+//             'Server' => 'laravelapiravvy-server.mysql.database.azure.com',
+//             'User Id' => 'upooeaawfx',
+//             'Password' => '4I1JC486PWOXR34G$'
             
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL', 'laravelapiravvy-server.mysql.database.azure.com'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravelapiravvy-database'),
+            'username' => env('DB_USERNAME', 'upooeaawfx'),
+            'password' => env('DB_PASSWORD', '4I1JC486PWOXR34G$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
